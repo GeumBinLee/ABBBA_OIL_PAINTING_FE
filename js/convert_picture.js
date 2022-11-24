b227dbee82568726ca17ab59caf21adf1c66c3cd
GeumBin <lgb9811@gmail.com>
2022-11-24 18:41:17 +0900

    :lipstick:Design: 이미지 변환 페이지 디자인 마무리 #2
    이미지 업로드 시 미리보기
    홈버튼 생성
    
    related_to : #2

--- js/convert_picture.js
+++ js/convert_picture.js
@@ -4,58 +4,78 @@ window.onload = () => {
 
     fakeUpload.addEventListener('click', () => realUpload.click());
 
+    function readImage(input) {
+        if (input.files && input.files[0]) {
+            const reader = new FileReader()
+
+            reader.onload = e => {
+                const previewImage = document.getElementById("slide")
+                previewImage.src = e.target.result
+            }
+            reader.readAsDataURL(input.files[0])
+        }
+    }
+    const inputImage = document.getElementById("input_image")
+    inputImage.addEventListener("change", e => {
+        readImage(e.target)
+    })
+    
 }
+
+
+
+
 
 const slide = document.querySelector("#slide");
 const imgList = ["https://c4.wallpaperflare.com/wallpaper/924/800/901/spider-man-artwork-ps4-red-wallpaper-preview.jpg",
-"https://wallpapercave.com/wp/wp9669554.jpg", "http://ojsfile.ohmynews.com/BIG_IMG_FILE/2016/1101/IE002043078_BIG.jpg"];
+    "https://wallpapercave.com/wp/wp9669554.jpg", "http://ojsfile.ohmynews.com/BIG_IMG_FILE/2016/1101/IE002043078_BIG.jpg"];
 let i = 0;
 const spider = document.querySelector("#spider")
 const witch = document.querySelector("#witch")
 const doctor = document.querySelector("#doctor")
 
-spider.onclick = function(){
+spider.onclick = function () {
     slide.src = imgList[0]
 }
-witch.onclick = function(){
+witch.onclick = function () {
     slide.src = imgList[1]
 }
-doctor.onclick = function(){
+doctor.onclick = function () {
     slide.src = imgList[2]
 }
 
-function prev(){
+function prev() {
     i--;
     slide.src = imgList[i];
-    if(i<0){
-        i = imgList.length-1;
+    if (i < 0) {
+        i = imgList.length - 1;
         slide.src = imgList[i];
     }
 }
 
-function next(){
+function next() {
     i++;
     slide.src = imgList[i];
-    if(i>= imgList.length){
-        i=0;
+    if (i >= imgList.length) {
+        i = 0;
         slide.src = imgList[i]
     }
 }
 
-function download(){
+function download() {
 
 }
 
 download = document.getElementById("download")
-download.onclick = function(){
+download.onclick = function () {
     // fetch(`http://127.0.0.1:8000/painters/download/${painting.id}`,{
-    response = fetch("http://127.0.0.1:8000/painters/download/2",{
-        headers:{
-            "authorization" : "Bearer " + localStorage.getItem("access")
+    response = fetch("http://127.0.0.1:8000/painters/download/2", {
+        headers: {
+            "authorization": "Bearer " + localStorage.getItem("access")
         },
-        method : 'GET'
+        method: 'GET'
     })
-    .then(response => response.json())
+        .then(response => response.json())
     console.log(response)
     alert("다운 완료!")
 }
