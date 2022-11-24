8275ef941f038f89f6484da0448defc4acf76e92
GeumBin <lgb9811@gmail.com>
2022-11-24 15:06:23 +0900

    :lipstick:Design: 이미지 변환 페이지 #2
    이미지 슬라이드로 넘어가게 함
    이미지 다운로드 버튼 연동했으나 백엔드 단에서 처리가 안 되는 듯함
    캐릭터 버튼 클릭시 사진 바뀜
    이미지 입력창 삽입
    전체적인 디자

--- js/convert_picture.js
+++ js/convert_picture.js
@@ -1,6 +1,61 @@
 window.onload = () => {
-const realUpload = document.querySelector('#input_image');
-const fakeUpload = document.querySelector('#upload_image');
+    const realUpload = document.querySelector('#input_image');
+    const fakeUpload = document.querySelector('#upload_image');
 
-fakeUpload.addEventListener('click', () => realUpload.click());
+    fakeUpload.addEventListener('click', () => realUpload.click());
+
+}
+
+const slide = document.querySelector("#slide");
+const imgList = ["https://c4.wallpaperflare.com/wallpaper/924/800/901/spider-man-artwork-ps4-red-wallpaper-preview.jpg",
+"https://wallpapercave.com/wp/wp9669554.jpg", "http://ojsfile.ohmynews.com/BIG_IMG_FILE/2016/1101/IE002043078_BIG.jpg"];
+let i = 0;
+const spider = document.querySelector("#spider")
+const witch = document.querySelector("#witch")
+const doctor = document.querySelector("#doctor")
+
+spider.onclick = function(){
+    slide.src = imgList[0]
+}
+witch.onclick = function(){
+    slide.src = imgList[1]
+}
+doctor.onclick = function(){
+    slide.src = imgList[2]
+}
+
+function prev(){
+    i--;
+    slide.src = imgList[i];
+    if(i<0){
+        i = imgList.length-1;
+        slide.src = imgList[i];
+    }
+}
+
+function next(){
+    i++;
+    slide.src = imgList[i];
+    if(i>= imgList.length){
+        i=0;
+        slide.src = imgList[i]
+    }
+}
+
+function download(){
+
+}
+
+download = document.getElementById("download")
+download.onclick = function(){
+    // fetch(`http://127.0.0.1:8000/painters/download/${painting.id}`,{
+    response = fetch("http://127.0.0.1:8000/painters/download/2",{
+        headers:{
+            "authorization" : "Bearer " + localStorage.getItem("access")
+        },
+        method : 'GET'
+    })
+    .then(response => response.json())
+    console.log(response)
+    alert("다운 완료!")
 }
