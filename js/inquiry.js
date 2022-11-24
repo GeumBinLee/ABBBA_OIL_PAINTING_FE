dc2b16b623d032713ef6d9017faa5c604866f6ab
GeumBin <107880480+GeumBinLee@users.noreply.github.com>
2022-11-24 21:15:06 +0900

    Revert ":art: Fix : inquiry.html,js,css(modal), Feat : inquiry_detail.html,css,jss"

--- js/inquiry.js
+++ js/inquiry.js
@@ -1,16 +1,20 @@
 window.onload = () => {
+    console.log("연결")
     handleinquiries()
 }
-function to_inquiry_detail(id) {
-    location.href = "inquiry_detail.html"
+function to_article_detail(id) {
+    location.href = "detail_post.html"
     localStorage.setItem("article_id", id);
 }
-
+function to_write_article() {
+    location.href = "upload_post.html"
+}
 
 async function handleinquiries(){
     const payload = localStorage.getItem("payload");
     const payload_parse = JSON.parse(payload)
     const user_id = payload_parse.user_id
+    console.log("함수연결")
    
     const response = await fetch ('http://127.0.0.1:8000/inquiries/',{
         headers : {
@@ -20,6 +24,7 @@ async function handleinquiries(){
     })
 
     response_json = await response.json()
+    console.log(response_json)
 
     
 
@@ -32,42 +37,13 @@ async function handleinquiries(){
         <td>${response_json[i].title}</td>
         <td>${response_json[i].nickname}</td>
         <td>${create_at}</td>
-        <th><button  onclick="to_inquiry_detail(${response_json[i].id});" class="bbtn_btn-dark">클릭</button></th>`;
+        <th><button onclick="to_article_detail(${response_json[i].id});">클릭</button></th>`;
 
         document.querySelector(".body").append(inquiry)
+        console.log(response_json[i]);
+        console.log(response_json[i].title);
+        console.log(response_json[i].content);
 
     }
 }
 
-const modal = document.querySelector('.modal');
-const modal_open = document.querySelector('.btn_btn-dark');
-const modal_close = document.querySelector('.modal_close');
-
-modal_open.addEventListener('click', () => {
-modal.style.display = 'block';
-});
-
-modal_close.addEventListener('click', () => {
-    modal.style.display = 'none';
-    });
-
-
-
-async function inquirypost() {
-    const content = document.getElementById("content").value
-    const title = document.getElementById("title").value
-
-    const response = await fetch('http://127.0.0.1:8000/inquiries/',{
-        headers:{
-            'content-type':'application/json',
-            "authorization" : "Bearer " + localStorage.getItem("access")
-        },
-        
-        method:'post',
-
-        body: JSON.stringify({
-            "title":title,
-            "content":content,
-        })
-    })
-}
